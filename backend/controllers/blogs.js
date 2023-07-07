97548b7c326925f1943a757ae33c7c43279593d7
janne kalliokulju <janne.kalliokulju@gmail.com>
2023-07-07 20:18:09 +0300

    13.17

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -1,26 +1,11 @@
 const jwt = require('jsonwebtoken')
-const { Op } = require('sequelize')
-const { SECRET } = require('../util/config')
-
 const router = require('express').Router()
 
-const { Blog, User } = require('../models')
+const { Op } = require('sequelize')
+const { SECRET } = require('../util/config')
+const { tokenExtractor } = require('../util/middleware')
 
-const tokenExtractor = (req, res, next) => {
-  const authorization = req.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    try {
-      console.log(authorization.substring(7))
-      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
-    } catch (error){
-      console.log(error)
-      return res.status(401).json({ error: 'token invalid' })
-    }
-  } else {
-    return res.status(401).json({ error: 'token missing' })
-  }
-  next()
-}
+const { Blog, User } = require('../models')
 
 const blogFinder = async (req, res, next) => {
     req.blog = await Blog.findByPk(req.params.id)
--- backend/util/middleware.js
+++ backend/util/middleware.js
@@ -13,5 +13,21 @@ const errorHandler = (error, request, response, next) => {
     next(error)
   }
 
-module.exports = {errorHandler} 
+  const tokenExtractor = (req, res, next) => {
+    const authorization = req.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      try {
+        console.log(authorization.substring(7))
+        req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+      } catch (error){
+        console.log(error)
+        return res.status(401).json({ error: 'token invalid' })
+      }
+    } else {
+      return res.status(401).json({ error: 'token missing' })
+    }
+    next()
+  }
+
+module.exports = {errorHandler, tokenExtractor} 
  
